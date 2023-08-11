029ab56c5756804653d1f14f13215c47a7336ff0
cl27 <cl27v684i@gmail.com>
2023-08-12 01:21:06 +0700

    Lesson #15.1 - View Classes: ListView
    Add class WomenHome, class WomenCategory in women/views.py
    Udate route in women/urls.py
    
     Changes to be committed:
    	modified:   women/urls.py
    	modified:   women/views.py

--- women/urls.py
+++ women/urls.py
@@ -3,11 +3,13 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', WomenHome.as_view(), name='home'),     # Отображение классом
+    # path('', index, name='home'),                 # Отображение функцией
     path('about/', about, name='about'),
     path('add_page/', add_page, name='add_page'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
     path('post/<slug:post_slug>/', show_post, name='post'),
-    path('catgory/<slug:cat_slug>/', show_category, name='category'),
+    path('catgory/<slug:cat_slug>/', WomenCategory.as_view(), name='category'),     # Отображение классом
+    # path('catgory/<slug:cat_slug>/', show_category, name='category'),             # Отображение функцией
 ]
--- women/views.py
+++ women/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView
 
 from .forms import *
 from .models import *
@@ -12,17 +13,37 @@ menu = [
 ]
 
 
-def index(request):
-    posts = Women.objects.all()
-    # cats = Category.objects.all()
-    context = {
-        'posts': posts,
-        # 'cats': cats,
-        'menu': menu,
-        'title': 'Главная страница',
-        'cat_selected': 0,
-    }
-    return render(request, 'women/index.html', context=context)
+# Отображение домашней страницы ("")
+class WomenHome(ListView):
+    model = Women
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
+    # extra_context = {'title': 'Главная страница'}       # Только для статического контента
+
+    # Для динамического (и статического) контента объявляется функция get_context_data
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Главная страница'
+        context['cat_selected'] = 0     # Выбрана "Все категории"
+        return context
+
+    # Отображать только "is_published=True"
+    def get_queryset(self):
+        return Women.objects.filter(is_published=True)
+
+
+# def index(request):
+#     posts = Women.objects.all()
+#     # cats = Category.objects.all()
+#     context = {
+#         'posts': posts,
+#         # 'cats': cats,
+#         'menu': menu,
+#         'title': 'Главная страница',
+#         'cat_selected': 0,
+#     }
+#     return render(request, 'women/index.html', context=context)
 
 
 def about(request):
@@ -60,16 +81,36 @@ def show_post(request, post_slug):
     return render(request, 'women/post.html', context=context)
 
 
-def show_category(request, cat_slug):
-    posts = Women.objects.filter(cat__slug=cat_slug)
-    context = {
-        'posts': posts,
-        # 'cats': cats,
-        'menu': menu,
-        'title': 'Отображение по рубрикам',
-        'cat_selected': cat_slug
-    }
-    return render(request, 'women/index.html', context=context)
+# Отображение категорий ('catgory/<slug:cat_slug>/')
+class WomenCategory(ListView):
+    model = Women
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
+    allow_empty = False     # 404 if empty
+
+    # Для динамического (и статического) контента объявляется функция get_context_data
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
+        context['cat_selected'] = context['posts'][0].cat_id     # Выбраная категория
+        return context
+
+    # Отображение (фильтр) только выбранной категории и "is_published=True"
+    def get_queryset(self):
+        return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+
+
+# def show_category(request, cat_slug):
+#     posts = Women.objects.filter(cat__slug=cat_slug)
+#     context = {
+#         'posts': posts,
+#         # 'cats': cats,
+#         'menu': menu,
+#         'title': 'Отображение по рубрикам',
+#         'cat_selected': cat_slug
+#     }
+#     return render(request, 'women/index.html', context=context)
 
 
 def pageNotFound(request, exception):
