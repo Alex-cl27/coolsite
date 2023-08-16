714c3ec3a2c9a18da0c852fd371adf73ea8102e5
cl27 <cl27v684i@gmail.com>
2023-08-16 14:07:17 +0700

    Lesson #21.1 - install Django debug toolbar
     Changes to be committed:
    	modified:   coolsite/settings.py
    	modified:   coolsite/urls.py
    	modified:   requirements.txt

--- coolsite/settings.py
+++ coolsite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',        # Django debug toolbar
     'women.apps.WomenConfig'
 ]
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',      # Django debug toolbar
 ]
 
 ROOT_URLCONF = 'coolsite.urls'
@@ -127,3 +129,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # LOGIN_REDIRECT_URL = '/'    # Дублируется в wiews.LoginUser.get_success_url()
+
+# Django debug toolbar
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
