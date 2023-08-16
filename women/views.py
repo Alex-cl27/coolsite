0312261e4236f8e3268a4de2d686f13f49cbd1d7
cl27 <cl27v684i@gmail.com>
2023-08-16 11:45:57 +0700

    Lesson #20.2 - User login
    Add custom user login form - forms.LoginUserForm. Fix 'title' in
    views.LoginUser. Change display forms in login.html - use 'for'.
    Add display username for authenticated user in base.html.
    Add views.logout_user, route for 'logout' - urls.urlpatterns.
    Authentication in case success registration user in
    views.RegisterUser.form_valid().
    
     Changes to be committed:
    	modified:   db.sqlite3
    	modified:   women/forms.py
    	modified:   women/templates/women/base.html
    	modified:   women/templates/women/login.html
    	modified:   women/urls.py
    	modified:   women/views.py

--- women/views.py
+++ women/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout, login
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
@@ -102,16 +102,27 @@ class RegisterUser(DataMixin, CreateView):
         c_def = self.get_user_context(title='Регистрация')     # Из .utils.DataMixin
         return dict(list(context.items()) + list(c_def.items()))
 
+    # Вызывается при успешной проверке формы регистрации
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
 
 class LoginUser(DataMixin, LoginView):
-    form_class = AuthenticationForm
+    form_class = LoginUserForm      # custom form
     template_name = 'women/login.html'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
         return dict(list(context.items()) + list(c_def.items()))
 
     # Логин редирект. Дублируется в settings.LOGIN_REDIRECT
     def get_success_url(self):
         return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
